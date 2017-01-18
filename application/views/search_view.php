2a26a3959d4e41820850d46b501a655cb464107c
Galredan <galredan@MacBook-Pro-de-Leo.local>
2017-01-18 13:43:02 +0100

    o

---
+++ application/views/search_view.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Research Page</title>
+	<link href="<?= constant('ASSETS') ?>css/flexslider.min.css" rel="stylesheet" type="text/css" media="all"/>
+	<link href="<?= constant('ASSETS') ?>css/line-icons.min.css" rel="stylesheet" type="text/css" media="all"/>
+	<link href="<?= constant('ASSETS') ?>css/elegant-icons.min.css" rel="stylesheet" type="text/css" media="all"/>
+	<link href="<?= constant('ASSETS') ?>css/lightbox.min.css" rel="stylesheet" type="text/css" media="all"/>
+	<link href="<?= constant('ASSETS') ?>css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all"/>
+	<link href="<?= constant('ASSETS') ?>css/theme-sunset.css" rel="stylesheet" type="text/css" media="all"/>
+	<link href="<?= constant('ASSETS') ?>css/custom.css" rel="stylesheet" type="text/css" media="all"/>
+</head>
+<body>
+<nav class="navbar navbar-default" role="navigation">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		<script src="https://www.youtube.com/iframe_api"></script>
+		<script src="<?= constant('ASSETS') ?>js/jquery.min.js"></script>
+        <script src="<?= constant('ASSETS') ?>js/jquery.plugin.min.js"></script>
+        <script src="<?= constant('ASSETS') ?>js/bootstrap.min.js"></script>
+        <script src="<?= constant('ASSETS') ?>js/jquery.flexslider-min.js"></script>
+        <script src="<?= constant('ASSETS') ?>js/smooth-scroll.min.js"></script>
+        <script src="<?= constant('ASSETS') ?>js/skrollr.min.js"></script>
+        <script src="<?= constant('ASSETS') ?>js/spectragram.min.js"></script>
+        <script src="<?= constant('ASSETS') ?>js/scrollReveal.min.js"></script>
+        <script src="<?= constant('ASSETS') ?>js/isotope.min.js"></script>
+        <script src="<?= constant('ASSETS') ?>js/twitterFetcher_v10_min.js"></script>
+        <script src="<?= constant('ASSETS') ?>js/lightbox.min.js"></script>
+        <script src="<?= constant('ASSETS') ?>js/jquery.countdown.min.js"></script>
+        <script src="<?= constant('ASSETS') ?>js/scripts.js"></script>
+</body>
+</html>
